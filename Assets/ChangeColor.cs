ddc05c1788d90d8b33f404ac17049416b9fe2e3a
Tejas Shroff <tejasshroff24@gmail.com>
2018-03-28 00:09:01 -0400

    Pushing project into separate repo

---
+++ Assets/ChangeColor.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColor : MonoBehaviour {
+
+    public Renderer rend;
+    public TextMesh textObject;
+
+	// Use this for initialization
+	void Start () {
+        rend = GetComponent<Renderer>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "BlueCube")
+        {
+            rend.material.color = Color.white;
+            textObject.text = "Hmmm. maybe?";
+        }
+    }
+}
